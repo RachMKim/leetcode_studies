803f4edfc61cacebbf26a942e9ed0cb13d054c96
Rachel Kim <rachelminjaekim@gmail.com>
2022-12-06 14:24:14 -0500

    first unique character

---
+++ first_unique_char.js
@@ -0,0 +1,36 @@
+/*
+Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+
+
+
+Example 1:
+
+Input: s = "leetcode"
+Output: 0
+Example 2:
+
+Input: s = "loveleetcode"
+Output: 2
+Example 3:
+
+Input: s = "aabb"
+Output: -1
+*/
+
+var firstUniqChar = function (s) {
+  let map = new Map();
+  for (let i = 0; i < s.length; i++) {
+    if (map.get(s[i])) {
+      map.set(s[i], map.get(s[i]) + 1);
+    } else {
+      map.set(s[i], 1);
+    }
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    if (map.get(s[i]) === 1) {
+      return i;
+    }
+  }
+  return -1;
+};
